e61249272b5f8c71590c996b8b2d9af37d5a6309
JonasVerbickas <jonasverbic@gmail.com>
2025-06-21 14:25:02 +0300

    ➕ Signal desktop app

--- setup.sh
+++ setup.sh
@@ -79,3 +79,13 @@ DEBIAN_FRONTEND=noninteractive sudo dpkg -i vscode.deb
 ## Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 DEBIAN_FRONTEND=noninteractive sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+## Signal
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg;
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+### Add Signal repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+### Update your package database and install Signal:
+sudo apt update && sudo apt install signal-desktop
+
